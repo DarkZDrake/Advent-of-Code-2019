98bfbdd8131bc1aa9d96c1fd69e1c414323f1a2e
DarkZDrake <ciaoc688@gmail.com>
2019-12-29 13:04:46 +0100

    day-3-end

--- day-5/main.js
+++ day-5/main.js
@@ -1,70 +1,76 @@
 const fs = require("fs");
 
 const input = fs
-  .readFileSync("input", { encoding: "utf8" })
-  .replace("\n", "")
-  .split(",")
-  .map(Number);
+    .readFileSync("input", {encoding: "utf8"})
+    .replace("\n", "")
+    .split(",")
+    .map(Number);
 
 /*const input = "1002,4,3,4,33";*/
 let intCode = input.slice();
 
 const gravityAssistant = input => {
-  let i = 0;
-  while (i < intCode.length) {
-    switch (intCode[i]) {
-      case 99:
-        return intCode[0];
-      case 3:
-        intCode[intCode[i + 1]] = input;
-        i += 2;
-        break;
-      default:
-        let opCode = immediateMode(intCode[i]).slice();
-        switch (opCode[0]) {
-          case 1:
-            intCode[intCode[i + 3]] =
-              getValue(opCode, 1, i) + getValue(opCode, 2, i);
-            i += 4;
-            break;
-          case 2:
-            intCode[intCode[i + 3]] =
-              getValue(opCode, 1, i) * getValue(opCode, 2, i);
-            i += 4;
-            break;
-          case 4:
-            console.log(getValue(opCode, 1, i));
-            i += 2;
-            break;
-          case 5:
-            i += 3;
-            break;
-          case 6:
-            i += 3;
-            break;
-          case 7:
-            i += 4;
-            break;
-          case 8:
-            i += 4;
-            break;
+    let i = 0;
+    while (i < intCode.length) {
+        switch (intCode[i]) {
+            case 99:
+                return intCode[0];
+            case 3:
+                intCode[intCode[i + 1]] = input;
+                i += 2;
+                break;
+            default:
+                let opCode = immediateMode(intCode[i]).slice();
+                switch (opCode[0]) {
+                    case 1:
+                        intCode[intCode[i + 3]] =
+                            getValue(opCode, 1, i) + getValue(opCode, 2, i);
+                        i += 4;
+                        break;
+                    case 2:
+                        intCode[intCode[i + 3]] =
+                            getValue(opCode, 1, i) * getValue(opCode, 2, i);
+                        i += 4;
+                        break;
+                    case 4:
+                        console.log(getValue(opCode, 1, i));
+                        i += 2;
+                        break;
+                    case 5:
+                        if (getValue(opCode, 1, i) !== 0) {
+                            i = getValue(opCode, 2, i);
+                            console.log(i);
+                        } else i += 3;
+                        break;
+                    case 6:
+                        if (getValue(opCode, 1, i) === 0) i = getValue(opCode, 2, i);
+                        else i += 3;
+                        break;
+                    case 7:
+                        intCode[intCode[i + 3]] = getValue(opCode, 1, i) < getValue(opCode, 2, i) ? 1 : 0;
+                        i += 4;
+                        break;
+                    case 8:
+                        intCode[intCode[i + 3]] = getValue(opCode, 1, i) === getValue(opCode, 2, i) ? 1 : 0;
+                        i += 4;
+                        break;
+                }
+                break;
         }
-        break;
     }
-  }
 };
 
 const immediateMode = code => {
-  let opcode = [];
-  opcode.push(code % 100);
-  opcode.push(Math.floor(code / 100) % 10);
-  opcode.push(Math.floor(code / 1000) % 10);
-  opcode.push(Math.floor(code / 10000) % 10);
-  return opcode;
+    let opcode = [];
+    opcode.push(code % 100);
+    opcode.push(Math.floor(code / 100) % 10);
+    opcode.push(Math.floor(code / 1000) % 10);
+    opcode.push(Math.floor(code / 10000) % 10);
+    return opcode;
 };
 
 const getValue = (opCode, index, i) => {
-  return opCode[index] === 0 ? intCode[intCode[i + index]] : intCode[i + index];
+    return opCode[index] === 0 ? intCode[intCode[i + index]] : intCode[i + index];
 };
 
-gravityAssistant(1);
+gravityAssistant(5);
